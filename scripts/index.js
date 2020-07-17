b3f12d197d4093680f0ce97917996e3791411c77
hojelse <k1@hojelse.dk>
2020-07-17 23:18:05 +0200

    Remove pop-in mediapost animation

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,68 @@
+// function browerWarnings() {
+//   let isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+//   // let iOS = navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
+//   // let iOS = true;
+//   if (isIOS) {
+//     alert("You are on an iOS device. Some functions might not work as intended.")
+//   }
+//   let isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+//   if(!isChrome) {
+//     alert("You are not using Google Chrome. Some functions might not work as intended.")
+//   }
+//   if(isChrome) {
+//     alert("chrome");
+//   }
+// }
+// browerWarnings();
+
+// function onVisibilityChange(el, callback) {
+//   var old_visible;
+//   return function () {
+//       var visible = isElementInViewport(el);
+//       if (visible != old_visible) {
+//           old_visible = visible;
+//           if (typeof callback == 'function') {
+//               callback();
+//           }
+//       }
+//   }
+// }
+
+// var handler = onVisibilityChange(el, function() {
+//   /* Your code go here */
+//   console.log(el);
+// });
+
+if (window.addEventListener) {
+  addEventListener('DOMContentLoaded', handler, false);
+  addEventListener('load', handler, false);
+  addEventListener('scroll', handler, false);
+  addEventListener('resize', handler, false);
+} else if (window.attachEvent)  {
+  attachEvent('onDOMContentLoaded', handler); // Internet Explorer 9+ :(
+  attachEvent('onload', handler);
+  attachEvent('onscroll', handler);
+  attachEvent('onresize', handler);
+}
+
+
+// let elems = document.querySelectorAll(".hidden")
+// for (let i = 0; i < elems.length; i++) {
+//   const element = elems[i];
+//   element.addEventListener
+// }
+
+function isElementInViewport (el) {
+  var rect = el.getBoundingClientRect();
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+
 function scrolldown() {
   $("html, body").animate(
     { scrollTop: $("section.mediaContainer").offset().top },
@@ -12,47 +77,47 @@ function mailto(URL) {
   }, 500)
 }
 
-var animateHTML = function () {
-  var elems
-  var windowHeight
-  function init() {
-    elems = document.querySelectorAll(".hidden")
-    windowHeight = window.innerHeight
-    addEventHandlers()
-    checkPosition()
-    fadeinElementsIfIOS()
-  }
-  function addEventHandlers() {
-    window.addEventListener("scroll", checkPosition)
-    window.addEventListener("resize", init)
-  }
-  function checkPosition() {
-    for (var i = 0; i < elems.length; i++) {
-      var positionFromTop = elems[i].getBoundingClientRect().top
-      if (positionFromTop - windowHeight <= 0) {
-        elems[i].className = elems[i].className.replace(
-          "hidden",
-          "fade-in-element"
-        )
-      }
-    }
-  }
-  function fadeinElementsIfIOS() {
-    let iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-    if (iOS) {
-      for (var i = 0; i < elems.length; i++) {
-        elems[i].className = elems[i].className.replace(
-          "hidden",
-          "fade-in-element"
-        )
-      }
-    }
-  }
-  return {
-    init: init,
-  }
-}
-animateHTML().init()
+// var animateHTML = function () {
+//   var elems
+//   var windowHeight
+//   function init() {
+//     elems = document.querySelectorAll(".hidden")
+//     windowHeight = window.innerHeight
+//     addEventHandlers()
+//     checkPosition()
+//     fadeinElementsIfIOS()
+//   }
+//   function addEventHandlers() {
+//     window.addEventListener("scroll", checkPosition)
+//     window.addEventListener("resize", init)
+//   }
+//   function checkPosition() {
+//     for (var i = 0; i < elems.length; i++) {
+//       var positionFromTop = elems[i].getBoundingClientRect().top
+//       if (positionFromTop - windowHeight <= 0) {
+//         elems[i].className = elems[i].className.replace(
+//           "hidden",
+//           "fade-in-element"
+//         )
+//       }
+//     }
+//   }
+//   function fadeinElementsIfIOS() {
+//     let iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+//     if (iOS) {
+//       for (var i = 0; i < elems.length; i++) {
+//         elems[i].className = elems[i].className.replace(
+//           "hidden",
+//           "fade-in-element"
+//         )
+//       }
+//     }
+//   }
+//   return {
+//     init: init,
+//   }
+// }
+// animateHTML().init()
 
 function footer() {
   var tryme = document.getElementById("tryme")
